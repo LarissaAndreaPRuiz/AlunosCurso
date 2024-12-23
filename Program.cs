af29e6ab7392889f636f90417e2a1f3b34129268
Larissa Ruiz <80786453+LarissaAndreaPRuiz@users.noreply.github.com>
2024-12-23 11:53:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.NetworkInformation;
+using Explorando.Models;
+
+Pessoa p1 = new Pessoa(nome: "Lari", sobrenome: "Ruiz");
+
+Pessoa p2 = new Pessoa(nome: "Ana", sobrenome: "Beatriz");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
